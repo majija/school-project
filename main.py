29ef8feeeb3fdc0e5406a2d0f51b35a4afed3f28
mpayson <mpayson@mail.ru>
2020-03-06 09:46:23 +0300

    Начал что-то колупать

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import telebot
+import sqlite3
 bot = telebot.TeleBot()  # Сюда нужно вставлять токен каждый раз
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, познакомимся?')
+
+"""
+@bot.message_handler(commands=['create_profile'])
+def create_user(message):
+    bot.send_message(message.chat.id, 'Привет, познакомимся?')
+    username = message.from_user.username
+    rating = 5.0
+    string_to_add = ('10, ' + str(rating) + ", '" + str(username) + "'")
+    cursor.execute("""INSERT INTO table1  (a, b, c)
+                   VALUES
+                   """)
+"""
 
 
 bot.polling()
